dece6bcbcc71c08af5ed5bc5851756ddd7c1d9e5
Jean Carlos HP <jeanchpaula@gmail.com>
2023-03-05 23:08:22 -0300

    Finalizado configuração inicial

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,21 @@
-import ReactFlow, { Background, Controls, BackgroundVariant, Node } from "reactflow"
+import { useCallback } from "react";
+import ReactFlow, { Background, Controls, BackgroundVariant, Node, ConnectionMode, useEdgesState, Connection, addEdge, useNodesState } from "reactflow"
+import * as Toolbar from '@radix-ui/react-toolbar'
 import 'reactflow/dist/style.css'
+import DefaultEdge from "./components/edges/DefaultEdge";
 
 import { Square } from './components/nodes/Square'
 
 const reactFlowStyle = {
-  background: '#37474F',
+  background: '#262626',
 };
 
 const NODE_TYPES = {
   square: Square,
+}
+
+const EDGE_TYPES = {
+  default: DefaultEdge,
 }
 
 const INITIAL_NODES = [
@@ -21,23 +28,91 @@ const INITIAL_NODES = [
     },
     data: {}
   },
+  {
+    id: crypto.randomUUID(),
+    type: 'square',
+    position: {
+      x: 650,
+      y: 250
+    },
+    data: {}
+  }
 ] satisfies Node[]
 
 function App() {
+  const [edges, setEdges, onChangeEdges] = useEdgesState([])
+  const [nodes, setNodes, onChangeNodes] = useNodesState(INITIAL_NODES)
+
+  const onConnect = useCallback((connection: Connection) => {
+      return setEdges(edges => addEdge(connection, edges))
+    }, []
+  )
+  
+  function addSquareNode() {
+    setNodes(nodes => [
+      ...nodes, 
+      {
+        id: crypto.randomUUID(),
+        type: 'square',
+        position: { 
+          x: 850,
+          y: 250
+        },
+        data: {}
+      }
+    ])
+  }
+
   return (
     <div style={{ width: '100vw', height: '100vh' }}>
       <ReactFlow 
         style={reactFlowStyle}
         nodeTypes={NODE_TYPES}
-        nodes={INITIAL_NODES}
+        edgeTypes={EDGE_TYPES}
+        nodes={nodes}
+        edges={edges}
+        onEdgesChange={onChangeEdges}
+        onConnect={onConnect}
+        onNodesChange={onChangeNodes}
+        connectionMode={ConnectionMode.Loose}
+        defaultEdgeOptions={{
+          type: 'default',
+        }}
       >
         <Background 
           variant={BackgroundVariant.Lines} 
           gap={10} 
-          color='#455A64'
+          color='#424242'
         />
         <Controls />
       </ReactFlow>
+
+      <Toolbar.Root style={{ 
+        position: "fixed", 
+        bottom: "20px", 
+        transform: "translateX(200%)",
+        backgroundColor: "#fff", 
+        border: "2px solid #cecece",
+        borderRadius: "16px", 
+        width: "24rem", 
+        height: "5rem", 
+        overflow: "hidden",
+        padding: "0.5rem", 
+      }}>
+        <Toolbar.Button 
+          style={{
+            backgroundColor: "#0e1929",
+            width: "5rem",
+            height: "5rem",
+            marginTop: "1rem",
+            borderRadius: "16px"
+          }}
+          className="toolbar-button"
+          onClick={addSquareNode}
+        >
+
+        </Toolbar.Button>
+      </Toolbar.Root>
     </div>
   )
 }
--- src/components/nodes/Square.tsx
+++ src/components/nodes/Square.tsx
@@ -1,9 +1,60 @@
-import { NodeProps } from 'reactflow'
+import { NodeResizer } from '@reactflow/node-resizer'
+import { NodeProps, Handle, Position } from 'reactflow'
+
+import "@reactflow/node-resizer/dist/style.css"
 
 export function Square(props: NodeProps) {
-  return (
-    <div style={{ backgroundColor: 'violet', width: '200px', height: '200px'}}>
+  const handleStyle = {
+    base: {
+      backgroundColor: '#1e3557', 
+      border: "1px solid #fff", 
+      width: '0.7rem', 
+      height: '0.7rem'
+    },
+    positionRight: {
+      marginRight: '-0.8rem',
+    },
+    positionLeft: {
+      marginLeft: '-0.8rem',
+    },
+    positionTop: {
+      marginTop: '-0.8rem',
+    },
+    positionBottom: {
+      marginBottom: '-0.8rem',
+    }
+  }
 
+  return (
+    <div style={{ backgroundColor: '#0e1929', width: "100%", height: "100%", minWidth: '200px', minHeight: '200px' }}>
+      <NodeResizer
+        minWidth={200}
+        minHeight={200}
+      />
+      <Handle
+        id="right"
+        type="source"
+        position={Position.Right}
+        style={{ ...handleStyle.base, ...handleStyle.positionRight }}
+      />
+      <Handle
+        id="left"
+        type="source"
+        position={Position.Left}
+        style={{ ...handleStyle.base, ...handleStyle.positionLeft }}
+      />
+      <Handle
+        id="top"
+        type="source"
+        position={Position.Top}
+        style={{ ...handleStyle.base, ...handleStyle.positionTop }}
+      />
+      <Handle
+        id="bottom"
+        type="source"
+        position={Position.Bottom}
+        style={{ ...handleStyle.base, ...handleStyle.positionBottom }}
+      />
     </div>
   )
 }
